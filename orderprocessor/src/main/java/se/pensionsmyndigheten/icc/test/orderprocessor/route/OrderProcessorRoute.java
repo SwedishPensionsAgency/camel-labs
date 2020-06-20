19b2e86ce1ff3b29c3148c37d4b4951450828974
admmajo <admmajo@K2733.svk.local>
2020-06-20 09:12:57 +0200

    Skapar en order per fil.

--- orderprocessor/src/main/java/se/pensionsmyndigheten/icc/test/orderprocessor/route/OrderProcessorRoute.java
+++ orderprocessor/src/main/java/se/pensionsmyndigheten/icc/test/orderprocessor/route/OrderProcessorRoute.java
@@ -17,42 +17,30 @@ public class OrderProcessorRoute extends RouteBuilder {
 
 	private static final String ROUTE_ID = "OrderProcessorRoute";
 
-	private static final int MAX_ORDERS_PER_FILE = 2;
+	private static final int MAX_NR_OF_ORDERS = 2;
+	
+	private static final String FILE_OUT_PATTERN = "${file:onlyname.noext}.${header.CamelSplitIndex}.${file:name.ext}";
 
 	@Override
 	public void configure() throws Exception {
 
 		onException(ValidationException.class)
-				.setHeader("CamelFileName", simple("badorder.${header.CamelSplitIndex}.${header.CamelFileName}"))
+				.setHeader("CamelFileName", simple(FILE_OUT_PATTERN))
 				.to("properties:invalid.uri");
 
-		// note, 'properties:somekey' could be replaced with '{{somekey}}'
-//		  from("properties:source.uri")
-//		  	.routeId(ROUTE_ID)
-//		  	.streamCaching()
-//		  	.log(LoggingLevel.INFO,"Processing file ${header.CamelFileName}")
-//		  	.split().tokenizeXML("order").streaming() //
-//		  	.log(LoggingLevel.INFO,"Split ${body}")
-//		  	.aggregate(constant(true), new BodyAggregatingStrategy())
-//		  	.completionSize(MAX_ORDERS_PER_FILE)
-//		  	.completionInterval(500) // TODO: Fix orders file ... // TODO: Validate //
-//		  	.log(LoggingLevel.INFO,"Validation success!") // TODO: Better file name
-//		  	.setHeader("CamelFileName", simple("orders-${header.CamelSplitIndex}.xml"))
-//		  	.to("properties:target.uri")
-//		  	.log(LoggingLevel.INFO, "Wrote file ${header.CamelFileName}") 
-//		  	.end();
-		  
 		Namespaces ns = new Namespaces("order", "http://test.icc.pensionsmyndigheten.se/order");
 
 		from("properties:source.uri")
-			.routeId(ROUTE_ID).streamCaching().split()
+			.routeId(ROUTE_ID)
+			.streamCaching()
+			.split()
 			.xtokenize("//order:order", 'w', ns)
 			.streaming()
 			.aggregate(constant(true), new BodyAggregatingStrategy())
 			.completionSize(1)
 			.completionInterval(500)
 			.to("properties:validation.uri").id("validate-input")
-			.setHeader("CamelFileName", simple("${header.CamelFileName}.${header.CamelSplitIndex}"))
+			.setHeader("CamelFileName", simple(FILE_OUT_PATTERN))
 			.to("properties:target.uri")
 			.end();
 
